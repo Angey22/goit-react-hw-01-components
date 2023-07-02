29fefddada220b74af33a318f571a2f9128003ee
Angey22 <babandval@gmail.com>
2023-07-02 03:38:02 +0300

    Added data files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,11 @@ import {
   Heading,
 } from 'components';
 
-// import data from 'data/data.json';
+// Importing data from JSON files
+// import user from '../../data/user.json';
+// import data from '../../data/data.json';
+// import friends from '../../data/friends.json';
+// import transactions from '../../data/transactions.json';
 
 export const App = () => {
   return (
